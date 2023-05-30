81e82f2d01ff6e67ca121d09b81ef86ede276318
Cristian Prado <127853816+Cristiansdeveloper@users.noreply.github.com>
2023-05-30 15:32:16 +0200

    First version

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+
+function App() {
+  return (
+
+
+    <div className="aplicacion-tareas">
+     <div className='tareas-lista-principal'>
+      <h1>Mis tareas</h1>
+        <ListaDeTareas />
+     </div>
+    </div>
+  );
+}
+
+export default App;
